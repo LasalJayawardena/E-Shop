1fa8a4de0921b77dd83f4db9c275f2e3db43236c
Lasal Jayawardena <doctor.lasalflair@gmail.com>
2021-09-04 12:58:22 +0530

    Added redux state tohomscreen

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -1,4 +1,4 @@
-import { axios } from 'axios';
+import axios from 'axios';
 import {
     PRODUCT_LIST_REQUEST,
     PRODUCT_LIST_SUCCESS,
@@ -11,6 +11,7 @@ export const listProducts = () => async (dispatch) => {
         dispatch({ type: PRODUCT_LIST_REQUEST });
 
         const { data } = await axios.get("/api/products");
+        console.log("IN")
         dispatch({ 
             type: PRODUCT_LIST_SUCCESS,
             payload: data 
