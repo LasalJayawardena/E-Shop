3186ef7ef0796cdd9051e3a997003007f1c14d4e
Lasal Jayawardena <doctor.lasalflair@gmail.com>
2021-09-07 23:56:30 +0530

    Added use auth endpoint

--- backend/server.js
+++ backend/server.js
@@ -2,21 +2,26 @@ import express from "express";
 import dotenv from "dotenv";
 import colors from "colors";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
-import connectDb from "./config/db.js";
+import connectDB from "./config/db.js";
 
 import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 
 dotenv.config();
 
-connectDb();
+connectDB();
 
 const app = express();
+
+// body parser to acess post req varables
+app.use(express.json());
 
 app.get("/", (req,res) => {
     res.send('APIS is running')
 });
 
 app.use('/api/products', productRoutes);
+app.use('/api/users/', userRoutes);
 
 // ======= Error middleware =======
 
